c9f018092a9e85641384ca4e4e7349aefaef018a
ckcclc <wchuang5900@163.cojm>
2017-10-26 01:21:01 +0800

    * anything  - fastJson * springboot  - errorCode, response

---
+++ springboot/src/main/java/com/ckcclc/springboot/common/Response.java
@@ -0,0 +1,60 @@
+package com.ckcclc.springboot.common;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by ckcclc on 26/10/2017.
+ */
+public class Response {
+
+    private int code = 0;
+    private String msg;
+    private Object result;
+
+    public Response() {
+    }
+
+    public Response(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.msg = errorCode.getMsg();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("error_code", code);
+        if (code != 0) {
+            jsonObject.put("msg", msg);
+        } else if (result != null){
+            String jsonString = JSON.toJSONString(result);
+            jsonObject.put("result", jsonString);
+        }
+        System.out.println(jsonObject.toJSONString());
+        return jsonObject.toJSONString();
+    }
+}
--- springboot/src/main/java/com/ckcclc/springboot/controller/TestController.java
+++ springboot/src/main/java/com/ckcclc/springboot/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.ckcclc.springboot.controller;
 
+import com.ckcclc.springboot.common.ErrorCode;
+import com.ckcclc.springboot.common.Response;
 import com.ckcclc.springboot.dao.PersonMapper;
 import com.ckcclc.springboot.entity.Person;
 import com.ckcclc.springboot.entity.Target;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ckcclc on 21/10/2017.
@@ -30,4 +35,26 @@ public class TestController {
     public Person person(@PathVariable("name") String name) {
         return personMapper.findByName(name);
     }
+
+    @RequestMapping("/successVal/{value}")
+    public String successVal(@PathVariable("value") String value) {
+        Response response = new Response();
+        Map<String, Object> map = new HashMap<>();
+        map.put("foo", 1);
+        map.put("bar", value);
+        response.setResult(map);
+        return response.toString();
+    }
+
+    @RequestMapping("/exception")
+    public String exception() {
+        return new Response(ErrorCode.UNKNOWN).toString();
+    }
+
+    @RequestMapping("/success")
+    public String success() {
+        return new Response().toString();
+    }
+
+
 }
